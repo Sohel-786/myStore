7af51f0361e718ebac279ad23e142a8dc485712b
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-03 16:34:51 +0530

    Working On User Layout

--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -1,24 +1,50 @@
 import { IoIosSearch } from "react-icons/io";
+import { FaRegCircleUser, FaCartShopping } from "react-icons/fa6";
+import { MdPersonAdd } from "react-icons/md";
 
 function UserLayout({ children }) {
   return (
-    <section>
-      <header className="flex justify-center items-center">
-        <div className="">
+    <section className="w-full max-w-[1280px] mx-auto">
+      <header className="flex max-w-[1280px] sticky justify-between items-center w-full px-5 py-1 border-b-2 border-gray-200 shadow-header">
+        <div className="flex items-center gap-3 w-[60%]">
           <img
             src="https://classroom-react.netlify.app/assets/classroom.svg"
             alt="logo"
             className="w-36 aspect-auto"
           />
-        </div>
-
-        <div className="bg-black rounded-lg flex justify-center items-center">
-          <IoIosSearch />
+        <div className="bg-black rounded-[5px] flex items-center px-1 py-[6px] w-[80%]">
+          <IoIosSearch size={"20px"} className="text-white mx-1 mt-[2px]" />
           <input
-            className="bg-transparent"
+            className="bg-transparent w-[95%] text-white border-none outline-none"
             type="text"
             placeholder="Search for Products, Brands and More"
           />
+        </div>
+        </div>
+
+
+        <div>
+          <span
+            type="button"
+            className="border-2 flex items-center gap-1 relative shadow-logBtn border-black rounded-[5px] cursor-pointer px-3 py-2 font-bold text-xs hover:text-white before:content-[''] before:right-full before:absolute before:top-0 before:bottom-0 before:left-0 before:bg-gray-800 before:-z-10 before:transition-all before:ease-in-out hover:before:right-0 lg:py-[5px] lg:px-3 lg:text-base"
+          >
+            <FaCartShopping size={"18px"} /> Cart
+          </span>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <span
+            type="button"
+            className="border-2 flex items-center gap-1 relative shadow-logBtn border-black rounded-[5px] cursor-pointer px-3 py-2 font-bold text-xs hover:text-white before:content-[''] before:right-full before:absolute before:top-0 before:bottom-0 before:left-0 before:bg-black before:-z-10 before:transition-all before:ease-in-out hover:before:right-0 lg:py-[5px] lg:px-3 lg:text-base"
+          >
+            <FaRegCircleUser size={"18px"} /> Login
+          </span>
+          <span
+            type="button"
+            className="border-2 flex items-center gap-1 relative shadow-logBtn border-black rounded-[5px] cursor-pointer px-3 py-2 font-bold text-xs hover:text-white before:content-[''] before:right-full before:absolute before:top-0 before:bottom-0 before:left-0 before:bg-black before:-z-10 before:transition-all before:ease-in-out hover:before:right-0 lg:py-[5px] lg:px-3 lg:text-base"
+          >
+            <MdPersonAdd size={"18px"} /> Sign Up
+          </span>
         </div>
       </header>
     </section>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow : {
+        logBtn : 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
+        header : 'rgba(33, 35, 38, 0.1) 0px 10px 10px -10px'
+      }
+    },
   },
   plugins: [],
 }
