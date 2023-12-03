02941762c25635389bd85d4b97ebb5b524b042e9
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-03 22:07:18 +0530

    Working On Landing Page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -20,34 +20,49 @@ function LandingPage() {
           />
 
           <span className="h-full flex justify-center items-center text-[#7f7f7f] hover:text-blue-500 border-l-[1.5px] w-[10%] hover:bg-gray-200 cursor-pointer">
-            <IoMdSearch size={'30px'} />
+            <IoMdSearch size={"30px"} />
           </span>
         </div>
-
       </div>
-        <div className="bg-white w-full">
-            <div className="w-[70%] py-4 mx-auto relative -top-10 bg-white rounded-t-3xl">
-                <ul className="flex justify-center items-center gap-8 font-Roboto font-semibold tracking-wide">
-                    <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">Home</li>
+      <div className="bg-white w-full">
+        <div className="w-[70%] py-4 mx-auto relative -top-10 bg-white rounded-t-3xl">
+          <ul className="flex justify-center items-center gap-8 font-Roboto font-semibold tracking-wide">
+            <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">
+              Home
+            </li>
 
-                    <li className="h-full text-gray-400 text-2xl relative top-[-2px]">|</li>
+            <li className="h-full text-gray-400 text-2xl relative top-[-2px]">
+              |
+            </li>
 
-                    <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">All Products</li>
-                    <li className="h-full text-gray-400 text-2xl relative top-[-2px]">|</li>
-                    <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">Men</li>
-                    <li className="h-full text-gray-400 text-2xl relative top-[-2px]">|</li>
-                    <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">Women</li>
-                    <li className="h-full text-gray-400 text-2xl relative top-[-2px]">|</li>
-                    <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">Kids</li>
-                </ul>
-            </div>
-
-            <div className="flex w-full h-[450px]">
-                <div>
-
-                </div>
-            </div>
+            <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">
+              All Products
+            </li>
+            <li className="h-full text-gray-400 text-2xl relative top-[-2px]">
+              |
+            </li>
+            <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">
+              Men
+            </li>
+            <li className="h-full text-gray-400 text-2xl relative top-[-2px]">
+              |
+            </li>
+            <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">
+              Women
+            </li>
+            <li className="h-full text-gray-400 text-2xl relative top-[-2px]">
+              |
+            </li>
+            <li className="relative font-bold pb-1 cursor-pointer before:absolute before:h-1 before:w-0 before:bottom-0 before:bg-black hover:before:w-full focus:before:w-full hover:before:duration-200 before:ease-in before:duration-300 hover:scale-110 transition-all duration-200">
+              Kids
+            </li>
+          </ul>
         </div>
+
+        <div className="flex w-full h-[450px]">
+          <div></div>
+        </div>
+      </div>
     </UserLayout>
   );
 }
