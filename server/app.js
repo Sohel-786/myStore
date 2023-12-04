0b803daf7c50de6bb5b5d7c820b2e953563b04d8
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-04 23:58:50 +0530

    Working On Backend

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     credentials: true,
-    exposedHeaders : ["set-cookie"],
+    exposedHeaders: ["set-cookie"],
   })
 );
 
--- server/server.js
+++ server/server.js
@@ -4,13 +4,11 @@ import app from "./app.js";
 
 import cloudinary from "cloudinary";
 
-
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   api_key: process.env.CLOUDINARY_API_KEY,
 });
-
 
 const PORT = process.env.PORT || 6020;
 
