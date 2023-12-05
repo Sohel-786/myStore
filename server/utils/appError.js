fd805f8e96744ee0f7f87507c1161fddd517aca8
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-05 23:59:19 +0530

    Working on Authentication

---
+++ server/utils/appError.js
@@ -0,0 +1,10 @@
+class AppError extends Error {
+    constructor (msg, statuscode){
+        super(msg);
+        this.statuscode = statuscode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default AppError;
