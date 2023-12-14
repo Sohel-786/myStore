1e5fb4d4bc687b54bbf7783a9553aa8ad363de9d
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-14 23:15:12 +0530

    Working on Product Entry

--- client/src/redux/slices/productSlice.js
+++ client/src/redux/slices/productSlice.js
@@ -6,30 +6,47 @@ const initialState = {
     products : []
 }
 
-export const addProduct = createAsyncThunk('/product/add' ,(data) => {
+export const addProduct = createAsyncThunk('/product/add' ,async (data) => {
     try {
         const res = axiosInstance.post('/product/', data);
         toast.promise(res, {
             pending : 'Wait, Adding New Product',
             success : 'Done',
             error: 'Something Went Wrong'
-        })
+        });
+
+        return await res;
     } catch (e) {
         toast.error(e?.response?.data?.message);
     }
-})
-export const getAllProducts = createAsyncThunk('/product/getAllProducts' ,() => {
+});
+
+export const getAllProducts = createAsyncThunk('/product/getAllProducts' ,async () => {
     try {
-        
+        const res = axiosInstance.get('/product/');
+        toast.promise(res, {
+            pending : 'Wait, Fetching All Products',
+            success : 'Done',
+            error: 'Something Went Wrong'
+        });
+
+        return await res;
     } catch (e) {
-        
+        toast.error(e?.response?.data?.message);
     }
 })
 
 const productSlice = createSlice({
     name : 'products',
     reducers: {},
-    initialState
+    initialState,
+    extraReducers : (builder) => {
+        builder.addCase(getAllProducts.fulfilled, (state, action) => {
+            if(action.payload){
+                state.products = action?.payload?.data?.allProducts ;
+            }
+        })
+    }
 });
 
 export default productSlice.reducer;
