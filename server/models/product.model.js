063d84e30457f00c18dfbca94dd2df74c5cee882
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-14 17:56:20 +0530

    Working on Product Entry

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,34 +1,40 @@
 import { Schema, model } from "mongoose";
 
 const productSchema = new Schema({
-    name : {
-        type : String,
-        required : ["true", "Product Name is Required"],
-        minLength: [5, "Product Name must be at least 5 characters"],
-        maxLength: [50, "Product Name must be less than 50 charcters"],
-        trim: true,
-        lowercase: true,
-    },
-    description : {
-        type : String,
-        required : ["true", "You Should Describe the Product you are adding."],
-        minLength : [10, "Product Description Should be long."],
-        trim: true,
-        lowercase : true,
-    },
-    price : {
-        type : String,
-        required : ["true", "The product must have price."]
-    }
-})
+  name: {
+    type: String,
+    required: ["true", "Product Name is Required"],
+    minLength: [5, "Product Name must be at least 5 characters"],
+    maxLength: [50, "Product Name must be less than 50 charcters"],
+    trim: true,
+    lowercase: true,
+  },
+  description: {
+    type: String,
+    required: ["true", "You Should Describe the Product you are adding."],
+    minLength: [10, "Product Description Should be long."],
+    trim: true,
+    lowercase: true,
+  },
+  price: {
+    type: Number,
+    required: ["true", "The product must have price."],
+  },
+  deliveryInfo: {
+    type: String,
+    required: ["true", "You should describe the cost of delivery."],
+    trim: true,
+    lowercase: true,
+  },
+  availableSizes: {},
+});
 
-
-name: "",
-          description: "",
-          category: "",
-          price: "",
-          deliveryInfo: "",
-          availableSizes: [],
-          sale: "no",
-          pricedrop: 0,
-          thumbnail: null,
+// name: "",
+//           description: "",
+//           category: "",
+//           price: "",
+//           deliveryInfo: "",
+//           availableSizes: [],
+//           sale: "no",
+//           pricedrop: 0,
+//           thumbnail: null,
