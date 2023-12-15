54861a5aa8ac670edf7f08408add7dddfdfb7637
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-15 15:05:39 +0530

    Working On Product Entry

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,55 +1,60 @@
 import { Schema, model } from "mongoose";
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    required: ["true", "Product Name is Required"],
-    minLength: [5, "Product Name must be at least 5 characters"],
-    maxLength: [50, "Product Name must be less than 50 charcters"],
-    trim: true,
-    lowercase: true,
-  },
-  description: {
-    type: String,
-    required: ["true", "You Should Describe the Product you are adding."],
-    minLength: [10, "Product Description Should be long."],
-    trim: true,
-    lowercase: true,
-  },
-  price: {
-    type: Number,
-    required: ["true", "The product must have price."],
-  },
-  deliveryInfo: {
-    type: String,
-    required: ["true", "You should describe the cost of delivery."],
-    trim: true,
-    lowercase: true,
-  },
-  availableSizes: {
-    type: Array,
-    required: ["true", "You must provide the correct size of the product."],
-  },
-  sale: {
-    type: String,
-    enum: ["NO", "YES"],
-    default: "NO",
-  },
-  pricedrop: {
-    type: Number,
-    default: 0,
-  },
-  thumbnail: {
-    public_id: {
+const productSchema = new Schema(
+  {
+    name: {
       type: String,
-      required: true,
+      required: ["true", "Product Name is Required"],
+      minLength: [5, "Product Name must be at least 5 characters"],
+      maxLength: [50, "Product Name must be less than 50 charcters"],
+      trim: true,
+      lowercase: true,
     },
-    secure_url: {
+    description: {
       type: String,
-      required: true,
+      required: ["true", "You Should Describe the Product you are adding."],
+      minLength: [10, "Product Description Should be long."],
+      trim: true,
+      lowercase: true,
+    },
+    price: {
+      type: Number,
+      required: ["true", "The product must have price."],
+    },
+    deliveryInfo: {
+      type: String,
+      required: ["true", "You should describe the cost of delivery."],
+      trim: true,
+      lowercase: true,
+    },
+    availableSizes: {
+      type: Array,
+      required: ["true", "You must provide the correct size of the product."],
+    },
+    sale: {
+      type: String,
+      enum: ["NO", "YES"],
+      default: "NO",
+    },
+    pricedrop: {
+      type: Number,
+      default: 0,
+    },
+    thumbnail: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      secure_url: {
+        type: String,
+        required: true,
+      },
     },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Product = model("product", productSchema);
 
