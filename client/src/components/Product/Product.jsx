0ca638d7891bccb9b852600835850cbb272a2b96
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-16 13:28:13 +0530

    Working On Product Card

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Product({ data }) {
+  const [showDetails, seShowDetails] = useState(true);
+
   let {
     name,
     description,
@@ -13,7 +17,7 @@ function Product({ data }) {
   } = data;
 
   if (sale === "YES") {
-   var finalprice = handleSalePrice(price, pricedrop);
+    var finalprice = handleSalePrice(price, pricedrop);
   }
 
   function handleSalePrice(price, off) {
@@ -21,35 +25,79 @@ function Product({ data }) {
     return Math.floor(price - temp);
   }
   return (
-    <li className="w-[210px] shadow-product flex flex-col cursor-pointer">
+    <li className="w-[210px] flex flex-col cursor-pointer hover:shadow-product">
       <div className="h-[280px] w-full">
         <img src={thumbnail.secure_url} alt={name} className="w-full h-full" />
       </div>
-      <div className="my-3 px-[10px]">
-        <div>
-          <h1 className="capitalize text-[#282c3f] mb-[6px] font-black font-Nova ">
-            {brand}
-          </h1>
-          <p
-            className="capitalize text-[#535766] text-[14px] leading-[14px] "
-            title={name}
-          >
-            {name.slice(0, 27)}..
-          </p>
-        </div>
+      {showDetails ? (
+        <div className="my-3 px-[10px] relative">
+          <div>
+            <p
+              className="capitalize text-[#535766] text-[13px] leading-[14px]"
+              title={name}
+            >
+              Sizes:{" "}
+              <span className="uppercase ml-[2px] font-Roboto">
+                {availableSizes.join(" , ")}
+              </span>
+            </p>
+          </div>
 
-        <div className="mt-[10px] mb-[6px] text-[#282c3f]">
-          {sale === "YES" ? (
-            <span>
-              {" "}
-              <span className="text-[14px] font-bold leading-[15px] ">Rs. {finalprice}</span> <span>Rs. {price}</span>{" "}
-              <span>({pricedrop}% OFF)</span>{" "}
-            </span>
-          ) : (
-            <></>
-          )}
+          <div className="mt-[10px] mb-[6px] text-[#282c3f]">
+            {sale === "YES" ? (
+              <span>
+                <span className="text-[14px] font-bold leading-[15px] ">
+                  Rs. {finalprice}
+                </span>
+                <span className="ml-[5px] text-[#7e818c] text-[12px] leading-[15px] line-through">
+                  Rs. {price}
+                </span>{" "}
+                <span className="text-[red] text-[12px] ml-[5px]">
+                  ({pricedrop}% OFF)
+                </span>{" "}
+              </span>
+            ) : (
+              <span className="text-[14px] font-bold leading-[15px] ">
+                Rs. {price}
+              </span>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="my-3 px-[10px]">
+          <div>
+            <h1 className="capitalize text-[#282c3f] mb-[6px] font-black font-Nova ">
+              {brand}
+            </h1>
+            <p
+              className="capitalize text-[#535766] text-[14px] leading-[14px] "
+              title={name}
+            >
+              {name.slice(0, 27)}..
+            </p>
+          </div>
+
+          <div className="mt-[10px] mb-[6px] text-[#282c3f]">
+            {sale === "YES" ? (
+              <span>
+                <span className="text-[14px] font-bold leading-[15px] ">
+                  Rs. {finalprice}
+                </span>
+                <span className="ml-[5px] text-[#7e818c] text-[12px] leading-[15px] line-through">
+                  Rs. {price}
+                </span>{" "}
+                <span className="text-[red] text-[12px] ml-[5px]">
+                  ({pricedrop}% OFF)
+                </span>{" "}
+              </span>
+            ) : (
+              <span className="text-[14px] font-bold leading-[15px] ">
+                Rs. {price}
+              </span>
+            )}
+          </div>
+        </div>
+      )}
     </li>
   );
 }
