cc0389ad21ae518e46ed818c22b9af4df423aaf7
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-20 16:40:01 +0530

    Working On Profile

--- server/routes/miscellaneous.routes.js
+++ server/routes/miscellaneous.routes.js
@@ -3,8 +3,6 @@ import { IsLoggedIn } from "../middlewares/auth.middleware.js";
 import { getCountriesStates } from "../controllers/miscellaneous.controller.js";
 const router = Router();
 
-router
-  .route("/")
-  .get(IsLoggedIn, getCountriesStates);
+router.route("/").get(IsLoggedIn, getCountriesStates);
 
 export default router;
