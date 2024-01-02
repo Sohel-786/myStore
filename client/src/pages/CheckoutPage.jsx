40b64b5b60e65e753629629f81dcd35e1a04d679
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-02 17:48:41 +0530

    Working on checkOut page

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -32,8 +32,22 @@ function CheckoutPage() {
                     state.map((el) => {
                         return (
                             <li key={nanoid(5)} className="flex rounded-md overflow-hidden w-full">
-                                <div className="">
-                                    <img src={el.product.thumbnail.secure_url} alt="" />
+                                <div className="w-[16%] h-[120px]">
+                                    <img src={el.product.thumbnail.secure_url} alt={el.product.name} className="w-full aspect-auto max-h-full" />
+                                </div>
+
+                                <div className="w-[84%] flex flex-col px-5 py-1">
+                                    <h1 className="capitalize font-semibold text-blue-600">{el.product.name}</h1>
+                                    <p className="capitalize font-bold font-Nova tracking-wide">{el.product.brand}</p>
+                                    <div className="w-full flex gap-6 mt-1 font-Roboto font-semibold ">
+                                        <h1>Pieces : <span className="font-Slab text-gray-500">{el.quantity}</span></h1>
+
+                                        <h1>Size : <span className="capitalize font-Slab text-gray-500">{el.size}</span></h1>
+
+                                        <h1>Price : <span className="capitalize font-Slab text-gray-500">Rs.{el.product.sale === 'YES' ? Math.floor(el.product.price - (el.product.pricedrop / 100) * el.product.price) : el.product.price}</span></h1>
+                                    </div>
+
+                                    <h1 className="font-Roboto font-semibold">Total : <span className="capitalize font-Slab text-gray-500">Rs. {el.price}</span></h1>
                                 </div>
                             </li>
                         )
