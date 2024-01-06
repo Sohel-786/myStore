9722cbfecc571d484acbbbc838e3d9d6f8076285
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-06 17:46:21 +0530

    working on payment gateway

---
+++ server/models/order.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const orderSchema = new Schema({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    orderItems : [
+        {
+            qty : {
+                type : Number, required : true
+            },
+            product : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : "Products"
+            },
+            currentPrice  : {
+                type : String,
+                required : true
+            }
+        }
+    ],
+    shippingAddress : {
+        name : {type : String, required : true},
+        address : {type : String, required : true},
+        city : {type : String, required : true},
+        state : {type : String, required : true},
+        postal : {type : String, required : true},
+        country : {type : String, required : true},
+    },
+    totalPrice : {type : Number, required : true},
+    isPaid : {type : Boolean, required : true},
+    isProcessing : {type : Boolean, required : true},
+}, {
+    timestamps : true
+});
