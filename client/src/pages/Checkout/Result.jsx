9ca75a8ab4a9f6e857750b327e69f0a8cb298334
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-09 16:55:04 +0530

    Working on Order part

--- client/src/pages/Checkout/Result.jsx
+++ client/src/pages/Checkout/Result.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 function Result() {
   const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.get("success"));
-  return <></>;
+  const [result, setResult] = useState(searchParams.get("success"));
+  return (
+    result && (
+      <>
+        {result === "false" ? (
+          <div className="w-full h-[90vh] flex flex-col justify-center items-center">
+            <h1 className="text-4xl font-semibold font-Nova tracking-wide text-[#35be35]">Payment Successful!</h1>
+            <img src="" alt="" />
+            <p className="font-bold font-Roboto tracking-wider text-gray-500">Thank you for shopping from Us</p>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </>
+    )
+  );
 }
 
 export default Result;
