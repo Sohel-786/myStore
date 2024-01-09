e2eaa54b62a1b940266ba4f075b4d004310a5205
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-09 16:55:16 +0530

    Working on Order part

--- client/src/pages/Checkout/Result.jsx
+++ client/src/pages/Checkout/Result.jsx
@@ -9,9 +9,13 @@ function Result() {
       <>
         {result === "false" ? (
           <div className="w-full h-[90vh] flex flex-col justify-center items-center">
-            <h1 className="text-4xl font-semibold font-Nova tracking-wide text-[#35be35]">Payment Successful!</h1>
+            <h1 className="text-4xl font-semibold font-Nova tracking-wide text-[#35be35]">
+              Payment Successful!
+            </h1>
             <img src="" alt="" />
-            <p className="font-bold font-Roboto tracking-wider text-gray-500">Thank you for shopping from Us</p>
+            <p className="font-bold font-Roboto tracking-wider text-gray-500">
+              Thank you for shopping from Us
+            </p>
           </div>
         ) : (
           <div></div>
