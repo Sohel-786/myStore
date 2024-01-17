2068f76b976842130e2ccb43cd459c5f32bfd8f1
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-17 14:46:46 +0530

    Working On orders page

--- client/src/components/OrderBox.jsx
+++ client/src/components/OrderBox.jsx
@@ -1,10 +1,22 @@
-function OrderBox({data}){
-    
-    return(
-        <div>
-
-        </div>
-    )
+function OrderBox({ data }) {
+  return (
+    <div className="w-[50%]">
+      <h1>#Order : {data._id}</h1>
+      <div className="w-full overflow-hidden flex ">
+        {data.orderItems.map((el) => {
+          return (
+            <div className="w-[20%] h-[150px] flex justify-center items-center">
+              <img src={el.product.thumbnail.secure_url} alt="Product Image" className="w-full h-full" />
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <button>In Process</button>
+        <button>View Details</button>
+      </div>
+    </div>
+  );
 }
 
 export default OrderBox;
--- client/src/pages/OrdersPage.jsx
+++ client/src/pages/OrdersPage.jsx
@@ -3,15 +3,17 @@ import UserLayout from "../layouts/UserLayout";
 import OrderBox from "../components/OrderBox";
 
 function Orders() {
-    const { orders } = useSelector((s) => s?.orderData);
+  const { orders } = useSelector((s) => s?.orderData);
 
-  return <UserLayout>
-      {
-        orders.map((el) => {
-          return <OrderBox data={el} />
-        })
-      }
-  </UserLayout>;
+  return (
+    <UserLayout>
+      <div className="flex w-full">
+        {orders.map((el) => {
+          return <OrderBox data={el} />;
+        })}
+      </div>
+    </UserLayout>
+  );
 }
 
 export default Orders;
