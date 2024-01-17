802c52a3b015ec815428f6cc7f7374907a6d80e3
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-17 15:00:59 +0530

    Working On orders page

--- client/src/components/OrderBox.jsx
+++ client/src/components/OrderBox.jsx
@@ -1,17 +1,21 @@
 function OrderBox({ data }) {
   return (
-    <div className="w-[50%] rounded-[10px] bg-orange-100">
-      <h1>#Order : {data._id}</h1>
+    <div className="w-[50%] rounded-[10px] bg-orange-50 px-5 py-4">
+      <h1 className="font-Slab font-bold text-sm mb-[10px] text-gray-700 tracking-wide">
+        #Order : {data._id}
+      </h1>
       <div className="w-full overflow-hidden flex ">
         {data.orderItems.map((el) => {
           return (
-            <div className="w-[20%] h-[150px] flex justify-center items-center">
-              <img
-                src={el.product.thumbnail.secure_url}
-                alt="Product Image"
-                className="w-full h-full"
-              />
-            </div>
+            <div
+              className="w-[20%] h-[90px] flex justify-center items-center rounded-[5px] overflow-hidden"
+              style={{
+                backgroundImage: `url(${el.product.thumbnail.secure_url})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+            ></div>
           );
         })}
       </div>
--- client/src/pages/OrdersPage.jsx
+++ client/src/pages/OrdersPage.jsx
@@ -7,7 +7,7 @@ function Orders() {
 
   return (
     <UserLayout>
-      <div className="flex w-[95%] gap-[20px] py-[30px] px-[40px] mx-auto border-[1px] border-black">
+      <div className="flex w-[99%] gap-[20px] py-[30px] px-[40px] mx-auto">
         {orders.map((el) => {
           return <OrderBox data={el} />;
         })}
