6b6105e721c68d39324172c591467ebdf580507b
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2024-01-24 19:22:15 +0530

    Working On filter functionalities

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -4,14 +4,7 @@ function Loading() {
   const location = useLocation();
   return (
     <div
-      className={`${
-        (location.pathname !== "/women" ||
-        location.pathname !== "/men" ||
-        location.pathname !== "/kids" ||
-        location.pathname !== '/all-products')
-          ? `w-[100vw] h-[100vh]`
-          : `w-[full] h-[2000px]`
-      } bg-white flex justify-center items-center`}
+      className={`bg-white flex w-[100vw] h-[100vh] justify-center items-center`}
     >
       <img src="/assets/loadingGif.gif" className="w-12" alt="Loading..." />
     </div>
--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -27,24 +27,24 @@ function AllProducts() {
         <div className="w-[20%] scroll overflow-y-scroll sticky h-[80vh] top-[180px] border-t-[1px] border-b-[1px] pt-2 pb-12 border-gray-200">
           {Allproducts && <Filter data={Allproducts} sort={handleSorting} />}
         </div>
-        <ul className="flex w-[80%] gap-5 justify-center flex-wrap gap-y-6">
+        <>
           {sortedProducts ? (
             sortedProducts.length > 0 ? (
-              <>
+              <ul className="flex w-[80%] gap-5 justify-center flex-wrap gap-y-6">
                 {" "}
                 {sortedProducts.map((el) => {
                   return <Product key={nanoid(4)} data={el} />;
                 })}{" "}
-              </>
+              </ul>
             ) : (
-              <div className="h-[50vh] w-full flex justify-center items-center">
+              <div className="h-[50vh] w-[80%] flex justify-center items-center">
                 <img src="/assets/Sorry.jpg" alt="Sorry No Product Found" className="max-w-full max-h-full" />
               </div>
             )
           ) : (
             <Loading />
           )}
-        </ul>
+        </>
       </div>
     </UserLayout>
   );
